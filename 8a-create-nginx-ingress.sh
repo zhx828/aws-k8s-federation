d92f21441718145cd6f0d770ca5dbade69caea83
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-05-10 18:45:19 -0400

    Update 8a-create-nginx-ingress.sh
    https://github.com/kubernetes-sigs/external-dns/blob/master/docs/faq.md#why-is-externaldns-only-adding-a-single-ip-address-in-route-53-on-aws-when-using-the-nginx-ingress-controller-how-do-i-get-it-to-use-the-fqdn-of-the-elb-assigned-to-my-nginx-ingress-controller-service-instead

--- 8a-create-nginx-ingress.sh
+++ 8a-create-nginx-ingress.sh
@@ -9,6 +9,6 @@ set -eux
 for c in ${US_EAST_CONTEXT} ${AP_NORTHEAST_CONTEXT}; do
     echo; echo ------------ ${c} : install nginx-ingress controller ------------; echo
     kubectl config use-context ${c}
-    helm install stable/nginx-ingress --name nginx-ingress
+    helm install stable/nginx-ingress --name nginx-ingress --publish-service=test/ingress-oncokb-dev
     echo; echo
 done
